a6b0245df9d113b3734750fb1c017f26ef9af945
Jiaxi Zhang <39978180+jiaxiZHANG@users.noreply.github.com>
2025-12-25 22:42:32 +0300

    fix(build): Gate Hypervisor framework linking to macOS target only

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,4 +1,7 @@
 fn main() {
-    println!("cargo:rustc-link-lib=framework=Hypervisor");
-    println!("cargo:rustc-link-search=framework=/System/Library/Frameworks");
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    if target_os == "macos" {
+        println!("cargo:rustc-link-lib=framework=Hypervisor");
+        println!("cargo:rustc-link-search=framework=/System/Library/Frameworks");
+    }
 }
